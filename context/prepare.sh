d4d5885f8a841ad1abbed808c7aeae65c9b3e868
Izzy Coding <matthew@thepinkmile.org.uk>
2024-02-20 08:00:15 +0000

    Re-organised build to ensure we activate the meta-toradex-security layer.

--- context/prepare.sh
+++ context/prepare.sh
@@ -9,13 +9,16 @@ echo "Running in: ${working_directory}"
 echo "Script directory: ${path}"
 echo "Config directory: ${config_directory}"
 
+source export
+
 if ! [ -d layers/meta-toradex-security ]; then
     git clone -b kirkstone-6.x.y https://github.com/toradex/meta-toradex-security.git layers/meta-toradex-security
 fi
-
-source export
+if ! grep -q "meta-toradex-security" "${config_directory}/bblayers.conf"; then
+    echo 'BBLAYERS += "${TOPDIR}/../layers/meta-toradex-security"' >> ${config_directory}/bblayers.conf
+fi
 
 # change the config
-sed -i 's@#MACHINE ?= "verdin-imx8mp"@MACHINE ?= "verdin-imx8mp"\nACCEPT_FSL_EULA = "1"\nINHERTT += "tdx-signed"\nTDX_IMX_HAB_CST_DIR="/opt/tools/cst-3.4.0/linux64/bin"\n@g' ${config_directory}/local.conf
+sed -i 's@#MACHINE ?= "verdin-imx8mp"@MACHINE ?= "verdin-imx8mp"\nACCEPT_FSL_EULA = "1"\nINHERTT += "tdx-signed"\n@g' ${config_directory}/local.conf
 sed -i 's@SSTATE_DIR ?= "${TOPDIR}/../sstate-cache"@SSTATE_DIR ?= "/opt/yocto-state"@g' ${config_directory}/local.conf
 sed -i 's@PACKAGE_CLASSES ?= "package_ipk"@PACKAGE_CLASSES ?= "package_deb"@g' ${config_directory}/local.conf
