81c2a2e42a0aba6fba993c00761275deb15c77ff
Izzy Coding <matthew@thepinkmile.org.uk>
2024-02-25 21:56:40 +0000

    Fix for issue with copying deploy output and missing fit image keys.

--- context/build.sh
+++ context/build.sh
@@ -10,17 +10,17 @@ reference_image=tdx-reference-minimal-image
 echo "Running in: ${working_directory}"
 echo "Script directory: ${path}"
 
-# copy previous output if provided
-if [ -f ${artifacts_directory}/verdin-image.tar.gz ]; then
-    echo "Extracting previous images..."
-    tar -xzvf ${artifacts_directory}/verdin-image.tar.gz -C ${working_directory}
-    rm -f ${artifacts_directory}/verdin-image.tar.gz
-fi
 # copy previous state
 if [ -f ${artifacts_directory}/yocto-state.tar.gz ]; then
     echo "Extracting previous state..."
     tar -xzvf ${artifacts_directory}/yocto-state.tar.gz -C /opt
     rm -f ${artifacts_directory}/yocto-state.tar.gz
+fi
+# copy previous fit image keys
+if [ -f ${artifacts_directory}/fit-keys.tar.gz ]; then
+    echo "Extracting previous fit image keys..."
+    tar -xzvf ${artifacts_directory}/fit-keys.tar.gz -C ${working_directory}
+    rm -f ${artifacts_directory}/fit-keys.tar.gz
 fi
 
 source export
@@ -28,6 +28,7 @@ source export
 bitbake -k ${reference_image}
 
 mkdir /opt/yocto-output
-tar -czvf /opt/yocto-output/yocto-state.tar.gz -C /opt yocto-state
+tar -czvf /opt/yocto-output/yocto-state.tar.gz -C /opt yocto-state/
 tar -czvf /opt/yocto-output/verdin-image.tar.gz -C ${working_directory} build/deploy/images/verdin-imx8mp/
 tar -czvf /opt/yocto-output/cst.tar.gz -C ${working_directory} cst
+tar -czvf /opt/yocto-output/fit-keys.tar.gz -C ${working_directory} build/keys/
