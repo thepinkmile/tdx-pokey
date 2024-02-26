034a772e00dfc89282b6ce5fffe7e9c7295f6b27
Izzy Coding <matthew@thepinkmile.org.uk>
2024-02-26 07:05:14 +0000

    reset set_state script back to working version.

--- set_yocto_state.bat
+++ set_yocto_state.bat
@@ -1,11 +1,5 @@
 @ECHO OFF
 PUSHD %~dp0
-	if exists ./output/yocto-state.tar.gz (
-		echo "Extracting build state"
-		docker cp ./output/yocto-state.tar.gz tdx-builder:/opt/artifacts/
-	)
-	if exists ./output/fit-keys.tar.gz (
-		echo "Extracting build fit image keys"
-		docker cp ./output/fit-keys.tar.gz tdx-builder:/opt/artifacts/
-	)
+	docker cp ./output/yocto-state.tar.gz tdx-builder:/opt/artifacts/
+	docker cp ./output/fit-keys.tar.gz tdx-builder:/opt/artifacts/
 POPD
